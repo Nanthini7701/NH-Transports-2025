214d57f0d3a4d1a16f0cc01b7923cee1e6b4c526
GitHub User <user@github.com>
2026-05-06 11:38:44 +0530

    fix database configuration

--- nhtransport/settings.py
+++ nhtransport/settings.py
@@ -72,9 +72,8 @@ WSGI_APPLICATION = 'nhtransport.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get("DATABASE_URL"),
+        default='sqlite:///db.sqlite3',
         conn_max_age=600,
-        ssl_require=False,
     )
 }
 
