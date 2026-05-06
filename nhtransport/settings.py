624f4ec26be7501d0c8401b50ee1ff235fac8946
GitHub User <user@github.com>
2026-05-06 10:39:15 +0530

    fix csrf trusted origin for railway

--- nhtransport/settings.py
+++ nhtransport/settings.py
@@ -13,13 +13,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django-insecure-your-secret-key-change-in-production-99999999999999999999')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*",  "nh-transports-2025-production.up.railway.app",
+ALLOWED_HOSTS = ["*",  "nh-transports2025.up.railway.app",
     "localhost",
     "127.0.0.1",]
 
-
+CSRF_TRUSTED_ORIGINS = [
+    "https://nh-transports2025.up.railway.app",
+]
 # Application definition
 
 INSTALLED_APPS = [
