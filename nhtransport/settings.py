abd9854c88cd36f2937056c977f9e7e76baae8fb
GitHub User <user@github.com>
2026-05-06 11:19:06 +0530

    fix csrf railway domain

--- nhtransport/settings.py
+++ nhtransport/settings.py
@@ -124,7 +124,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-
+EMAIL_TIMEOUT = 20
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', 'nhtransports2025@gmail.com')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', 'ovyc asaq dcgt ncqb')
 
